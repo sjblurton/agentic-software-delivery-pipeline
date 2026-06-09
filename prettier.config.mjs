64c6b5cd86a70624ce54c5f254229d3f87576ee9
Simon Blurton <sblurton@hotmail.co.uk>
2026-06-09 19:30:15 +0100

    refactor: update configuration files for improved organization and sorting

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -2,7 +2,7 @@ import organizeImports from "prettier-plugin-organize-imports";
 
 const config = {
   plugins: [organizeImports, "prettier-plugin-sort-json"],
-  jsonRecursiveSort: false,
+  jsonRecursiveSort: true,
   jsonSortOrder: JSON.stringify({ [/.*/]: "lexical" }),
 };
 

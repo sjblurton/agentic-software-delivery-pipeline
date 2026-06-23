b8536575d28019c627302dcf128aa8081cdc9744
Simon Blurton <sblurton@hotmail.co.uk>
2026-06-23 16:52:21 +0100

    chore: update .prettierignore and eslint config to include additional directories; modify lint script in package.json

--- src/components/ui/field-description/field-description.tsx
+++ src/components/ui/field-description/field-description.tsx
@@ -9,7 +9,7 @@ function FieldDescription({ className, ...props }: React.ComponentProps<"p">) {
       data-slot="field-description"
       className={cn(
         `text-sm leading-normal font-normal text-muted-foreground
-        group-has-[[data-orientation=horizontal]]/field:text-balance`,
+        group-has-data-[orientation=horizontal]/field:text-balance`,
         "last:mt-0 nth-last-2:-mt-1 [[data-variant=legend]+&]:-mt-1.5",
         "[&>a]:underline [&>a]:underline-offset-4 [&>a:hover]:text-primary",
         className,
